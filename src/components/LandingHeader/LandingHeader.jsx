bce9be4a7a35dc802f539a8d111755d7a1632742
leonelborgesdev <leonel03t@gmail.com>
2024-04-05 14:26:18 -0400

    animate write

--- src/components/LandingHeader/LandingHeader.jsx
+++ src/components/LandingHeader/LandingHeader.jsx
@@ -9,11 +9,11 @@ export const LandingHeader = () => {
         <span style={{ "--i": "0" }} data-text={" Coders."}>
           Coders.
         </span>
-        <span style={{ "--i": "1" }} data-text={" Developers."}>
-          Developers.
+        <span style={{ "--i": "1" }} data-text={" FullStackDevelopers."}>
+          FullStackDevelopers.
         </span>
-        <span style={{ "--i": "2" }} data-text={" Designers."}>
-          Designers.
+        <span style={{ "--i": "2" }} data-text={" Developers."}>
+          Developers.
         </span>
       </h2>
     </div>
