66b208e642d284cdb4fc50031b7b4345925da390
leonelborgesdev <leonel03t@gmail.com>
2024-04-05 14:54:26 -0400

    navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { LandingHeader } from "./components/LandingHeader/LandingHeader";
+import { Nav } from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <LandingHeader />
     </div>
   );
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Nav.css";
+
+export const Nav = () => {
+  return (
+    <div className="Navigation">
+      <span>Home</span>
+      <span>Services</span>
+      <span>Project</span>
+      <span>Contact</span>
+    </div>
+  );
+};
