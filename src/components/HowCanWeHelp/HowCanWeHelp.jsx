7c4eaf1e22ace6afcd2812eb43479cebb05fc264
leonelborgesdev <leonel03t@gmail.com>
2024-04-06 16:40:58 -0400

    who can we help styles

--- src/components/HowCanWeHelp/HowCanWeHelp.jsx
+++ src/components/HowCanWeHelp/HowCanWeHelp.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import mejoresSoluciones from "../../assets/mejoresSoluciones.jpg";
+import "./HowCanWeHelp.css";
 
 export const HowCanWeHelp = () => {
   return (
     <div className="containerHowCanWeHelp">
-      <div>
-        <h2>Whow Can We Help</h2>
+      <div className="weHelpText">
+        <h2>Como Podemos Ayudar</h2>
+        <p>
+          Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+          archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+          las industrias desde el año 1500, cuando un impresor (N. del T.
+          persona que se dedica a la imprenta) desconocido usó una galería de
+          textos y los mezcló de tal manera que logró hacer un libro de textos
+          especimen.
+        </p>
       </div>
-      <div>
+      <div className="weHelpImage">
         <img src={mejoresSoluciones} alt="" />
       </div>
     </div>
