7619621e0f95106773844b3938f7a4f2df4f5d36
leonelborgesdev <leonel03t@gmail.com>
2024-04-05 22:05:15 -0400

    services cards

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./OurServices.css";
+import graficos from "../../assets/graficos.gif";
+import laptop from "../../assets/laptop.gif";
+import soporte from "../../assets/soporte.gif";
 
 export const OurServices = () => {
   return (
     <div className="container_services">
       <div className="title_services">
+        <h2>We Provide Best</h2>
         <h2>
-          We Provide Best <span>Solutions</span> for you
+          <span>Solutions</span> for you
         </h2>
+      </div>
+      <div className="services">
+        <div className="card_service">
+          <img src={laptop} alt="" />
+          <h2>Web Developer</h2>
+        </div>
+        <div className="card_service">
+          <img src={soporte} alt="" />
+          <h2>Soprte Tecnico</h2>
+        </div>
+        <div className="card_service">
+          <img src={graficos} alt="" />
+          <h2>Analisis de Datos</h2>
+        </div>
       </div>
     </div>
   );
